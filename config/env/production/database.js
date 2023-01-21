bb94f05cc2b7097a76cf658df58ad341fc72a881
dhramvir73 <dharmvirkumar7321@gmail.com>
2023-01-21 18:41:00 +0530

    first-commit

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,19 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'dpg-cf5tl41mbjsmchd2f840-a.singapore-postgres.render.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'blog_t9gl'),
+        user: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', 'pTDX8YiLnzTxZEyZhXbBnTJLzbikiubm'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', true), // For self-signed certificates
+        },
       },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  });
+
+
